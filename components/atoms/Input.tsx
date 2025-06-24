6424a134c7fa7df36cbdad537d6e9d408f791c2d
berk145 <dereberk1@gmail.com>
2025-06-25 02:35:41 +0300

    Creating input component

---
+++ components/atoms/Input.tsx
@@ -0,0 +1,74 @@
+import { Colors } from "@/constants/theme";
+import React, { useState } from "react";
+import { StyleSheet, TextInput, TextInputProps, View } from "react-native";
+import { Typography } from "./Typography";
+
+type IProps = TextInputProps & {
+  label?: string;
+  charCount?: boolean;
+  maxLength?: number;
+  value: string;
+  onChangeText: (text: string) => void;
+  placeholder?: string; // Optional placeholder
+};
+
+export const Input = ({
+  charCount = true,
+  maxLength = 500,
+  value,
+  placeholder,
+  onChangeText,
+  ...rest
+}: IProps) => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  return (
+    <View style={[styles.container, isFocused && styles.containerFocused]}>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={value}
+        multiline={true}
+        maxLength={maxLength}
+        autoCorrect={false}
+        placeholder={placeholder}
+        placeholderTextColor={Colors.placeholder}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        keyboardAppearance="dark"
+        {...rest}
+      />
+      {charCount && (
+        <Typography style={styles.charCounter} variant="caption">
+          {value.length}/{maxLength}
+        </Typography>
+      )}
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: 175,
+    backgroundColor: Colors.inputBackground,
+    padding: 12,
+    borderRadius: 16,
+  },
+
+  input: {
+    flex: 1,
+    borderRadius: 16,
+    textAlignVertical: "top",
+    color: Colors.white,
+    fontFamily: "Manrope-Regular",
+    fontSize: 16,
+    lineHeight: 21,
+    verticalAlign: "middle",
+  },
+  containerFocused: {
+    borderColor: Colors.white,
+  },
+  charCounter: {
+    color: Colors.placeholder,
+  },
+});
